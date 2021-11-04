571bcbdb840d83770bade64e0968c2e5e5cf1402
Maurizio <maurizio.bussi.mb@gmail.com>
2021-11-04 21:50:09 +0100

    reset python tests module

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,6 +1,6 @@
 import queue
 import unittest
-from pythongit import git
+from pythongit.core import git
 
 
 class TestGit(unittest.TestCase):
@@ -15,85 +15,6 @@ class TestGit(unittest.TestCase):
 
     def test_git_commands_queue(self):
         self.assertIsInstance(obj=self.vcs.commands, cls=queue.Queue)
-
-    def test_git_status_command_in_queue(self):
-        self.vcs._status()
-        self.assertIn(member="git status", container=self.vcs.commands.get())
-
-    def test_git_push_command_in_queue(self):
-        self.vcs._push()
-        self.assertIn(member="git push", container=self.vcs.commands.get())
-
-    def test_git_branch_command_in_queue(self):
-        self.vcs._branch()
-        self.assertIn(member="git branch", container=self.vcs.commands.get())
-
-    def test_git_checkout_command_in_queue(self):
-        self.vcs._checkout()
-        self.assertIn(member="git checkout", container=self.vcs.commands.get())
-
-    def test_git_add_command_in_queue(self):
-        self.vcs._add()
-        self.assertIn(member="git add", container=self.vcs.commands.get())
-
-    def test_git_commit_command_in_queue(self):
-        self.vcs._commit()
-        self.assertIn(member="git commit", container=self.vcs.commands.get())
-
-    def test_git_stash_command_in_queue(self):
-        self.vcs._stash()
-        self.assertIn(member="git stash", container=self.vcs.commands.get())
-
-    def test_git_bundle_command_in_queue(self):
-        self.vcs._bundle()
-        self.assertIn(member="git bundle", container=self.vcs.commands.get())
-
-    def test_git_has_shell_command(self):
-        self.assertTrue(hasattr(self.vcs, "shell"))
-
-    def test_git_shell_common_procedure(self):
-        self.vcs._status()
-        self.vcs._add()
-        self.vcs._commit()
-        self.assertIn(member="git status", container=self.vcs.commands.get())
-        self.assertIn(member="git add", container=self.vcs.commands.get())
-        self.assertIn(member="git commit", container=self.vcs.commands.get())
-
-    def test_git_shell_common_procedure_negative(self):
-        self.vcs._status()
-        self.vcs._add()
-        self.vcs._commit()
-        self.assertIn(member="git status", container=self.vcs.commands.get())
-        self.assertNotIn(member="git commit", container=self.vcs.commands.get())
-        self.assertNotIn(member="git add", container=self.vcs.commands.get())
-
-    def test_git_shell_tricky_procedure(self):
-        self.vcs._status()
-        self.vcs._stash()
-        self.vcs._stash()
-        self.vcs._add()
-        self.vcs._commit()
-        self.vcs._bundle()
-        self.assertIn(member="git status", container=self.vcs.commands.get())
-        self.assertIn(member="git stash", container=self.vcs.commands.get())
-        self.assertIn(member="git stash", container=self.vcs.commands.get())
-        self.assertIn(member="git add", container=self.vcs.commands.get())
-        self.assertIn(member="git commit", container=self.vcs.commands.get())
-        self.assertIn(member="git bundle", container=self.vcs.commands.get())
-
-    def test_git_shell_tricky_procedure_negative(self):
-        self.vcs._status()
-        self.vcs._stash()
-        self.vcs._stash()
-        self.vcs._add()
-        self.vcs._commit()
-        self.vcs._bundle()
-        self.assertIn(member="git status", container=self.vcs.commands.get())
-        self.assertNotIn(member="git bundle", container=self.vcs.commands.get())
-        self.assertNotIn(member="git commit", container=self.vcs.commands.get())
-        self.assertNotIn(member="git stash", container=self.vcs.commands.get())
-        self.assertNotIn(member="git add", container=self.vcs.commands.get())
-        self.assertNotIn(member="git stash", container=self.vcs.commands.get())
 
 
 if __name__ == '__main__':
